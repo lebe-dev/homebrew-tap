0bb82ece43eae51645a5d442eeab57d5d522db7e
Eugene Lebedev <eugene.0x90@gmail.com>
2026-06-13 22:04:49 +0300

    Add submarine v0.15.0

--- Formula/submarine.rb
+++ Formula/submarine.rb
@@ -1,9 +1,9 @@
 class Submarine < Formula
   desc "Tiny toolkit for LLM-powered subtitle translation workflows"
   homepage "https://github.com/lebe-dev/submarine"
-  url "https://github.com/lebe-dev/submarine/releases/download/0.14.0/sm-0.14.0-macos-arm64.zip"
-  version "0.14.0"
-  sha256 "871e2ffe15febcf044321c301cfcf975a5e20f33fbf889c6615042ef1fb80541"
+  url "https://github.com/lebe-dev/submarine/releases/download/0.15.0/sm-0.15.0-macos-arm64.zip"
+  version "0.15.0"
+  sha256 "6a2c2428c1e5c031fdb502353743534f2818132b95d402d01676140fdb16f6cb"
   license "MIT"
 
   def install
@@ -11,6 +11,6 @@ class Submarine < Formula
   end
 
   test do
-    assert_match "sm 0.14.0", shell_output("#{bin}/sm --version")
+    assert_match "sm 0.15.0", shell_output("#{bin}/sm --version")
   end
 end
